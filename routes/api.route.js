99296b89ca8fb276794f5c776a32d694b82b7297
trupnil <trupnil.b@xcitech.in>
2023-03-24 14:19:16 +0530

    experiment

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import TodoRoutes from "../app/api/Todo/Todo.routes.js";
 const router = Router();
-router.use("/todo",TodoRoutes); //todo routes added
+
 export default router;
