35602d0e64a70bd709393c0265e437682b7a6790
trupnil <trupnil.b@xcitech.in>
2024-04-12 16:36:49 +0530

    middleware changes

--- middlewares/jwt-auth.middleware.js
+++ middlewares/jwt-auth.middleware.js
@@ -1,22 +1,20 @@
 import jwt from "jsonwebtoken"
-            exports.tokenVerify = function(req,res,next){
-               try{
-                    let secretKey = process.env.JWT_SECRET_KEY;
-                    let token =req.headers['authorization'].split(' ')[1] //Remove for Bearer word
-                    if(token){
-                        if(jwt.verify(token,secretKey)){
-                           next();
-                        }else{
-                            res.status(401).json({auth:false,"message":"Unauthorised token"})
-                        }
-                    }
-                    else
-                    {
-                      throw new Error({"message":"Unauthorised token"});
-                    }
-                }
-                catch(e){
-                    res.status(401).json({auth:false,"message":"Unauthorised token"})
-                }
+export const tokenVerify = function (req, res, next) {
+    try {
+        let secretKey = process.env.JWT_SECRET_KEY;
+        let token = req.headers['authorization'].split(' ')[1] //Remove for Bearer word
+        if (token) {
+            if (jwt.verify(token, secretKey)) {
+                next();
+            } else {
+                res.status(401).json({ auth: false, "message": "Unauthorised token" })
             }
-            
+        }
+        else {
+            throw new Error({ "message": "Unauthorised token" });
+        }
+    }
+    catch (e) {
+        res.status(401).json({ auth: false, "message": "Unauthorised token" })
+    }
+}
